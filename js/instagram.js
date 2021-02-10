ae42fa492b17f908b2e17a42c528b2bd70813d8a
podoit <podoit.kr@gmail.com>
2021-02-10 18:59:37 +0900

    some

--- js/instagram.js
+++ js/instagram.js
@@ -1,56 +1,21 @@
+var xhttp = new XMLHttpRequest();
 var token = "IGQVJWVWdValdTdFNMUHgzSlVrX2lEdURWTHpreHlBdTU5WEtOZAktzZAThENE1RM0lOdlhtSWVfNlpuNkV6QldYNnhRLWZAlN2FlSnhhSjhrSGNmakxVU1VzT1NYUnhsMEpEejVVdWhEN1RyMlZANZAmpubwZDZD";
 
-$.ajax({
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var obj = JSON.parse(this.responseText);
+    console.log(obj);
 
-    type: "GET",
-    dataType: "jsonp",
-    cache: false,
-    url: "https://graph.instagram.com/me/media?access_token=" + token + "&fields=id,caption,media_type,media_url,thumbnail_url,permalink",
-
-    success: function(response) {
-
-        if (response.data != undefined && response.data.length > 0) {
-
-            for (i = 0; i < 6; i++) {
-
-                if (response.data[i]) {
-                    var item = response.data[i];
-
-                    if (item.media_type === "VIDEO"){
-                    image_url = item.thumbnail_url;
-                    }
-
-                    else {
-                    image_url = item.media_url;
-                    }
-                    
-                    var post = "<div class='instagram-"+ i +"'><a target='_blank' href=' " + item.permalink + "'><img src='"+ image_url+"'></a></div>"   
-   
-                    $('#myInstagram').append(post);
-                }
-
-                else {
-                    // if no curent item
-                    show_fallback('#insta-item-'+i)
-                }
-
-            }
-
-        }
-
-        else {
-        // if api error
-        show_fallback('.instagram-item')
-        }
-    },
-
-    error :function() {
-    // if http error
-    show_fallback('.instagram-item')
+    for(i=0;i<6;i++) {
+      if(obj.data[i].media_type == "VIDEO") {
+        obj.data[i].media_url = obj.data[i].thumbnail_url
+      }
+      document.getElementById("myInstagram").innerHTML +=  
+      "<a href='" + obj.data[i].permalink + "' target='_blank' rel='noopener noreferrer'>"
+      + "<div class='post' style='background-image:url("+ obj.data[i].media_url +")'></div></a>"
     }
-});
+  }
+};
 
-
-function show_fallback(el) {
-    $(el).addClass('loaded fallback');
-}
+xhttp.open("GET", "https://graph.instagram.com/me/media?access_token=" + token + "&fields=id,caption,media_type,media_url,thumbnail_url,permalink", true);
+xhttp.send();
